060d3ab5202a9f37b88fd6998ea984a026a38b90
Leena2003 <99492322+Leena2003@users.noreply.github.com>
2022-10-23 01:00:20 +0100

    corrected spelling errors

--- employee.py
+++ employee.py
@@ -2,11 +2,11 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name, comissions, comission_pay, bonus_comission, salary, hours=0):
+    def __init__(self, name, commissions, commission_pay, bonus_commission, salary, hours=0):
         self.name = name
-        self.comissions = comissions
-        self.comission_pay = comission_pay
-        self.bonus_comission = bonus_comission
+        self.commissions = commissions
+        self.commission_pay = commission_pay
+        self.bonus_commission = bonus_commission
         self.salary = salary
         self.hours = hours
 
@@ -15,18 +15,18 @@ class Employee:
 
         if self.hours == 0:
             total += self.salary
-            if self.comissions!=0:
-                total += (self.comissions*self.comission_pay)
-            elif self.bonus_comission !=0:
-                total += self.bonus_comission
+            if self.commissions!=0:
+                total += (self.commissions*self.commission_pay)
+            elif self.bonus_commission !=0:
+                total += self.bonus_commission
             return total
 
         else:
             total += (self.salary * self.hours)
-            if self.comissions != 0:
-                total += (self.comissions * self.comission_pay)
-            elif self.bonus_comission != 0:
-                total += self.bonus_comission
+            if self.commissions != 0:
+                total += (self.commissions * self.commission_pay)
+            elif self.bonus_commission != 0:
+                total += self.bonus_commission
             return total
 
 
@@ -35,13 +35,13 @@ class Employee:
         if self.hours !=0: #if hourly worker
             final_string += "contract of " + str(self.hours) + " hours at " + str(self.salary) + "/hour"
 
-            if self.comissions > 0:
-                final_string += " and recieves a comission for " + str(self.comissions) + " contract(s) at " + str(
-                    self.comission_pay) + "/contract. Their total pay is " + str(self.get_pay()) + "."
+            if self.commissions > 0:
+                final_string += " and receives a commission for " + str(self.commissions) + " contract(s) at " + str(
+                    self.commission_pay) + "/contract. Their total pay is " + str(self.get_pay()) + "."
 
-            elif (self.bonus_comission != 0):
-                final_string += " and recieves a bonus comission of " + str(
-                    self.bonus_comission) + ". Their total pay is " + str(self.get_pay()) + "."
+            elif (self.bonus_commission != 0):
+                final_string += " and receives a bonus commission of " + str(
+                    self.bonus_commission) + ". Their total pay is " + str(self.get_pay()) + "."
 
             else:
                 final_string += ". Their total pay is " + str(self.get_pay()) + "."
@@ -49,13 +49,13 @@ class Employee:
             return final_string
         else:
             final_string += "monthly salary of " + str(self.salary)
-            if self.comissions > 0:
-                final_string += " and recieves a comission for " + str(self.comissions) + " contract(s) at " + str(
-                    self.comission_pay) + "/contract. Their total pay is " + str(self.get_pay()) + "."
+            if self.commissions > 0:
+                final_string += " and receives a commission for " + str(self.commissions) + " contract(s) at " + str(
+                    self.commission_pay) + "/contract. Their total pay is " + str(self.get_pay()) + "."
 
-            elif (self.bonus_comission != 0):
-                final_string += " and recieves a bonus comission of " + str(
-                    self.bonus_comission) + ". Their total pay is " + str(self.get_pay()) + "."
+            elif (self.bonus_commission != 0):
+                final_string += " and receives a bonus commission of " + str(
+                    self.bonus_commission) + ". Their total pay is " + str(self.get_pay()) + "."
 
             else:
                 final_string += ". Their total pay is " + str(self.get_pay()) + "."
@@ -66,12 +66,20 @@ billie = Employee('Billie',0,0,0,4000)
 
 charlie = Employee('Charlie', 0, 0, 0, 25, 100)
 
+
 renee = Employee('Renee', 4, 200, 0, 3000)
+
+
 
 jan = Employee('Jan', 3, 220, 0, 25, 150)
 
+
 robbie = Employee('Robbie', 0, 0, 1500, 2000)
 
+
 ariel = Employee('Ariel', 0, 0, 600, 30, 120)
+
+
+
 
 
